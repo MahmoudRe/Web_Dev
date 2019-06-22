e97419562853d6c5cc36e163ef60f4ec337e7cdc
Mahmoud Re <m.alhumsialrefaai@student.tudelft.nl>
2019-06-22 12:18:32 +0200

    clean + reorder the repo

--- chessmaster-omar/public/javascripts/socket.js
+++
@@ -1,9 +0,0 @@
-var socket = new WebSocket("ws://localhost:3000");
-
-socket.onmessage = function (event) {
-    document.getElementById("header").innerHTML = event.data;
-};
-socket.onopen = function () {
-    let msg = [document.getElementById("board"), board_arr] 
-    socket.send(msg);
-};
